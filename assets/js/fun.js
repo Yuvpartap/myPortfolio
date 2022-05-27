b1a5ad1a33edaeba6fe2cba138793f042fc85710
Yuvpartap Singh Klair <yuvpartapsingh@gmail.com>
2022-05-27 23:13:29 +0530

    My Portfolio Website

---
+++ assets/js/fun.js
@@ -0,0 +1,68 @@
+function c_e(x)
+{
+    x.src = "images/Stat/stat73.png";
+    x.setAttribute('style','margin-bottom: 10px')
+}
+function js_e(x)
+{
+    x.src = "images/Stat/stat72.png";
+}
+function py_e(x)
+{
+    x.src = "images/Stat/stat75.png";
+}
+function html_e(x)
+{
+    x.src = "images/Stat/stat91.png";
+}
+function css_e(x)
+{
+    x.src = "images/Stat/stat87.png";
+}
+function ds_e(x)
+{
+    x.src = "images/Stat/stat57.png";
+}
+function sql_e(x)
+{
+    x.src = "images/Stat/stat65.png";
+}
+function git_e(x)
+{
+    x.src = "images/Stat/stat77.png";
+}
+
+/* onhoverout*/ 
+
+function c(x)
+{
+    x.src = "/images/Stat/cpp.png";
+}
+function js(x)
+{
+    x.src = "/images/Stat/js.png";
+}
+function py(x)
+{
+    x.src = "/images/Stat/python.png";
+}
+function html(x)
+{
+    x.src = "/images/Stat/html.png";
+}
+function css(x)
+{
+    x.src = "/images/Stat/css.png";
+}
+function ds(x)
+{
+    x.src = "/images/Stat/DS2.png";
+}
+function sql(x)
+{
+    x.src = "/images/Stat/SQL.png";
+}
+function git(x)
+{
+    x.src = "/images/Stat/git.png";
+}
